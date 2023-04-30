0bc6ddcd986cbed92971e27683f79d882582dedd
Rex Jacob <rexajacob@gmail.com>
2023-04-30 22:28:12 +0530

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const isAuth = require("./middleware/is-auth");
 const mongoose = require("mongoose");
 const graphQlSchema = require("./graphql/schema/index");
 const graphQlResolvers = require("./graphql/resolvers/index");
-
+const PORT = process.env.PORT || 6010;
 const app = express();
 
 app.use(bodyParser.json());
@@ -34,7 +34,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nuj3o2f.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority"`
   )
   .then(() => {
-    app.listen(8000);
+    app.listen(PORT);
   })
   .catch((err) => {
     console.log(err);
